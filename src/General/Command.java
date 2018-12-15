83933495d15319781640f441995639b06b4d4a53
micare <michael.arenander@gmail.com>
2018-12-15 22:55:07 +0100

    Initial commit

---
+++ src/General/Command.java
@@ -0,0 +1,143 @@
+package General;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class Command 
+{
+	Robot bot;
+	int mouseX = 100,
+		mouseY = 100;
+	
+	double 	screen[] = {0,0},
+			ratio[] = {1,1}; //monitor ratio for when screen resolution and cursor location don't match
+	
+	public Command () throws AWTException {
+		bot = new Robot();
+		//check screenRange
+		bot.mouseMove(10000,10000);
+		bot.waitForIdle();
+		screen[0] = MouseInfo.getPointerInfo().getLocation().getX();
+		screen[1] = MouseInfo.getPointerInfo().getLocation().getY();
+		System.out.println("Screen: "+screen[0]+" "+screen[1]);
+		bot.mouseMove(mouseX, mouseY);
+	}
+	
+	public String setRes(int x, int y) { ratio[0]= screen[0]/x; ratio[1]=screen[1]/y; return ratio[0]+" "+ratio[1]; }
+	public Color getColor(int x, int y) { return bot.getPixelColor(x, y); }
+	
+	public String mouseClick(int input) {
+		bot.mousePress(input);
+		bot.waitForIdle();
+		bot.delay(10);
+		bot.mouseRelease(input);
+		bot.waitForIdle();
+		
+		return "mouse clicked";
+	}
+	
+	public String mouseClick(int[] input)
+	{
+		for( int i : input) {
+			bot.mousePress(i);
+			bot.waitForIdle();
+			bot.delay(10);
+			bot.mouseRelease(i);
+			bot.waitForIdle();
+			bot.delay(10);
+		}
+		return "mouse clicked";
+	}
+	
+	public String keyType(char[] letter)
+	{
+		for( int i : letter) {
+			boolean capital = Character.isUpperCase(i);
+			if (capital) bot.keyPress(KeyEvent.VK_SHIFT);
+			else i = Character.toUpperCase(i);
+			bot.keyPress(i);
+			bot.waitForIdle();
+			bot.delay(10);
+			bot.keyRelease(i);
+			bot.waitForIdle();
+			bot.delay(10);
+			if (capital) bot.keyRelease(KeyEvent.VK_SHIFT);
+			
+		}
+		return "mouse clicked";
+	}
+	
+	public String keyType(char letter) {
+		Character.toUpperCase(letter);
+		bot.keyPress(letter);
+		bot.waitForIdle();
+		bot.delay(10);
+		bot.keyRelease(letter);
+		bot.waitForIdle();
+		
+		return "Typed: "+letter;
+	}
+	
+	public String mouseMove(int x, int y) {
+		double 	initial_loc[] = { 	MouseInfo.getPointerInfo().getLocation().getX(),
+									MouseInfo.getPointerInfo().getLocation().getY() },
+				target_loc[] = {	x*ratio[0], y*ratio[1] },
+				diff[] = {			target_loc[0] - initial_loc[0], target_loc[1] - initial_loc[1] };
+		
+		int max = 0,
+			interval = 40;
+		
+		while ( Math.abs(diff[0]) > 1 && Math.abs(diff[1]) > 1) {
+			if( Math.abs(diff[0]) <= 3) mouseX = (int) target_loc[0];
+			else mouseX = mouseX + (int) ((Math.abs(diff[0]) > interval)? diff[0]/interval : Math.signum(diff[0]));
+				
+			if( Math.abs(diff[1]) <= 3) mouseY = (int) target_loc[1];
+			else mouseY = mouseY + (int) ((Math.abs(diff[1]) > interval)? diff[1]/interval : Math.signum(diff[1]));
+			
+				diff[0] = target_loc[0] - mouseX;
+				diff[1] = target_loc[1] - mouseY;
+				
+			bot.mouseMove(mouseX, mouseY);
+			bot.waitForIdle();
+			bot.delay(1000/120);
+			max++;
+			if(max > 1000 || diff[0] > screen[0] || diff[1] > screen[1])
+				break;
+		}
+		
+		return "mouse moved from ("+initial_loc[0]+","+initial_loc[1]+") to ("+mouseX+","+mouseY+") ";
+	}
+	
+	public String type(String message) {
+		int input = 0;
+		
+		bot.keyPress(input);
+		bot.waitForIdle();
+		bot.delay(10);
+		bot.keyRelease(input);
+		bot.waitForIdle();
+		
+		return "Typed: "+message;
+	}
+	
+	public String paste(String message) {
+		StringSelection stringSelection = new StringSelection(message);
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(stringSelection, stringSelection);
+
+		bot.keyPress(KeyEvent.VK_CONTROL);
+		bot.keyPress(KeyEvent.VK_V);
+		bot.keyRelease(KeyEvent.VK_V);
+		bot.keyRelease(KeyEvent.VK_CONTROL);
+		
+		return ("mouse types '"+message+"'");
+	}
+}
---
+++ src/General/Main.java
@@ -0,0 +1,33 @@
+package General;
+
+import java.awt.AWTException;
+import java.awt.event.InputEvent;
+
+public class Main
+{
+	public static void main (String[] args)
+	{
+		taubot();
+	}
+	
+	public static void taubot()
+	{
+		Command c = null;
+		try { c = new Command(); }
+		catch (AWTException e) { e.printStackTrace(); }
+		c.setRes(100, 100);
+		
+		gwBot(c);
+	}
+	
+	public static void gwBot(Command c) {
+		log( c.mouseMove(15,90) );
+		log( c.mouseClick(InputEvent.BUTTON2_DOWN_MASK) );
+		log( c.mouseClick(InputEvent.BUTTON2_DOWN_MASK) );
+		log( c.mouseClick(InputEvent.BUTTON2_DOWN_MASK) );
+	}
+	
+	public static void log(String message) { System.out.println(message); }
+	public static void log(String message, boolean bcase) { if (bcase) System.out.println(message); }
+	public static void log(int[] val, boolean bcase) { if (bcase) for (int v : val) System.out.print(" ("+v+")"); }
+}
